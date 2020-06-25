05b16601686f4d9bb7341da2d2bd90479f3bdb88
Beekasha <brian.j.kleiner@gmail.com>
2020-06-25 14:08:51 -0400

    moved getmovies() inside MoviesAdapter

--- src/adapters/MoviesAdapter.js
+++ src/adapters/MoviesAdapter.js
@@ -2,8 +2,9 @@ class MoviesAdapter {
     constructor() {
         this.baseUrl = 'localhost:3000/movies'
     }
+
+    getmovies() {
+        return fetch(this.baseUrl).then(res => res.json())
+    }
 }
 
-getmovies() {
-    return fetch(this.baseUrl).then(res => res.json())
-}
