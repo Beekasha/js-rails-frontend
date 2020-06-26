2270dd87de66e15987f98271a4d67d3148635cea
Beekasha <brian.j.kleiner@gmail.com>
2020-06-26 14:04:15 -0400

    added to the Movie constructor

--- src/components/movie.js
+++ src/components/movie.js
@@ -2,6 +2,14 @@ class Movie {
     constructor(movieJSON) {
         this.id = movieJSON.id
         this.poster = movieJSON.poster
+        this.title = movieJSON.title
+        this.year = movieJSON.year
+        this.rated = movieJSON.rated
+        this.runtime = movieJSON.runtime
+        this.director = movieJSON.director
+        this.plot = movieJSON.plot
+        this.response = movieJSON.response
+        this.actors = movieJSON.actors
     }
 
     renderLi() {
